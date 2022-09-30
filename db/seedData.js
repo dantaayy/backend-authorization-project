fbacec5105ea9a4e97d1f1d867331c4ea24135a0
dantaayy <dantaayy@gmail.com>
2022-09-30 10:38:43 -0700

    got server up and running with basic webpage

---
+++ db/seedData.js
@@ -0,0 +1,55 @@
+const users = [
+    {
+        id: "9cc87812-184a-46ae-8650-e7172462464f",
+        isAdmin: false,
+        age: 29,
+        name: "Collins Burch",
+        employee: "SONIQUE",
+        email: "collinsburch@sonique.com",
+        phone: "+1 (998) 564-2710",
+        address: "857 Rockaway Avenue, Hall, Alaska, 7098"
+    },
+    {
+        id: "e259dfea-ca31-4a3b-9909-81416a69c937",
+        isAdmin: true,
+        age: 38,
+        name: "Mayer Graves",
+        employee: "ZILLA",
+        email: "mayergraves@zilla.com",
+        phone: "+1 (925) 580-2220",
+        address: "830 Wilson Street, Somerset, Maine, 505"
+    },
+    {
+        id: "dd66ef37-a4a8-4c73-a831-59e4efc63e9a",
+        isAdmin: false,
+        age: 20,
+        name: "Gayle Daniel",
+        "gender": "female",
+        employee: "FUTURITY",
+        email: "gayledaniel@futurity.com",
+        phone: "+1 (935) 447-2173",
+        address: "645 Anna Court, Dragoon, Hawaii, 6220"
+    },
+    {
+        id: "220cd780-04c1-406b-a449-463b160128c5",
+        isAdmin: true,
+        age: 31,
+        name: "Rosalyn Curtis",
+        "gender": "female",
+        employee: "ZOSIS",
+        email: "rosalyncurtis@zosis.com",
+        phone: "+1 (895) 463-3120",
+        address: "174 Bragg Street, Stouchsburg, Montana, 3884"
+    },
+    {
+        id: "a3f6eb5c-edaa-48e2-943d-92c4483704db",
+        isAdmin: false,
+        age: 25,
+        name: "Jeri Diaz",
+        "gender": "female",
+        employee: "AMTAP",
+        email: "jeridiaz@amtap.com",
+        phone: "+1 (813) 551-3540",
+        address: "167 Anthony Street, Hayden, Wyoming, 3523"
+    }
+]
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+// ROUTER TO SEND AND PASS BACK TO SERVER
+const router = express.Router()
+
+router.get('users', (req, res) => {
+    res.send(`<h1>User Page</h1>`)
+})
+
+module.exports = router
