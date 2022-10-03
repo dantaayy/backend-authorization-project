b731b194a3c8408e3dd0b824181b8120df617a50
dantaayy <dantaayy@gmail.com>
2022-10-03 09:52:48 -0700

    added seed function to load data

--- server.js
+++ server.js
@@ -1,32 +1,10 @@
-const express = require("express")
-const colors = require("colors")
-const path = require("path")
-const morgan = require("morgan")
+const app = require('./index')
+const {sequelize} = require('./db')
 
-// GRAB PATHS FROM FILE
-const routes = require('./src/routes/index')
-
-// INITIALIZE EXPRESS
-const app = express()
-
-const PORT = process.env.PORT
-
-// MIDDLEWARE
-app.use(morgan('dev'))
-
-// SET UP PUBLIC WEBPAGE ACCESS
-app.use(express.static(path.join(__dirname, 'src', 'public')))
-
-// DESTINATION TO SEND THIS REQUEST
-app.get('/', (req, res) => {
-    res.send('./index.html')
-})
-
-// ROUTES WITH / IN URL
-app.use('/api', routes)
+const { PORT = 4000 } = process.env
 
 app.listen(PORT || 8000, () => {
-    console.log(colors.rainbow(`
+    console.log(colors.inverse.blue(`
     🚀  Server is running!
     🔉  Listening on port ${PORT}
     📭  Webpage at http://localhost:8000`
